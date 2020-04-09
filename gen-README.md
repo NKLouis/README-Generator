21db43b25834dd26f87d8a910eb9f45aae11e527
naritchaya louis <naritchayalouis@gmail.com>
2020-04-09 18:19:33 -0500

    testing

--- gen-README.md
+++ gen-README.md
@@ -1,19 +1,48 @@
 
     
-    <a href="https://img.shields.io/badge/License-www-green">Badge</a>
+    <img src= "https://img.shields.io/badge/License-zzzzzzzzzzzz-green">
 
+    <h1> Readme Genterato-</h1>
+ 
+    <img src="https://avatars3.githubusercontent.com/u/58704859?v=4">
     
-    https://avatars1.githubusercontent.com/u/2239103?v=4
-    
-    www
-    ww
-    w
-    
-    ww
-    www
-    ww
-    ww
-    ww
-    ww
-    undefined 
+
+
+    <h2> Table of Contents </h2>
+    <li><a href="#title">Project</a></li>
+    <li><a href="#description">Description</a></li>  
+    <li><a href="#contents">Contents</a></li> 
+    <li><a href="#installation">Installation</a></li> 
+    <li><a href="#usage">Usage</a></li> 
+    <li><a href="#license">License</a></li> 
+    <li><a href="#contributor">Contributor</a></li>   
+    <li><a href="#tests">Tests</a></li> 
   
+
+
+    <h2 id="description"> Description </h2>
+    <p>zzzzzzzzzzz</p>
+
+    <h2 id="description"> contents </h2>
+    <p>zzzzzzzzzz</p>
+
+    <h2 id="description"> installation </h2>
+    <p>zzzzzzzzzzz</p>
+
+    <h2 id="description"> usage </h2>
+    <p>zzzzzzzzzzzz</p>
+
+    <h2 id="description"> license </h2>
+    <p>zzzzzzzzzzzz</p>
+
+    <h2 id="description"> contributor </h2>
+    <p>zzzzzzzzzzzz</p>
+
+    <h2 id="description"> tests </h2>
+    <p>zzzzzzzzzzz</p>
+
+    <h2 id="email"> Email </h2>
+    <a href= "Email:Naritchayalouis@Gmail.com">Naritchayalouis@Gmail.com</a> 
+
+    <h2 id="portfolio"> Portfolio </h2>
+    <a href= "https://nklouis.github.io/Portfolio/">Portfolio</a> 
--- index.js
+++ index.js
@@ -104,27 +104,57 @@ inquirer.prompt([
 
   function generateMarkdown(response, res) {
 
-    const usersInfo = `
+    const getInfo = `
     
-    <a href="https://img.shields.io/badge/License-${response.license}-green">Badge</a>
+    <img src= "https://img.shields.io/badge/License-${response.license}-green">
 
+    <h1> ${response.title}</h1>
+ 
+    <img src="${res.data.avatar_url}">
     
-    ${res.data.avatar_url}
-    
-    ${response.title}
-    ${response.description}
-    ${response.contents}
-    ${response.installation}
-    ${response.usage}
-    ${response.license}
-    ${response.contributor}
-    ${response.tests}
-    ${response.linkedin}
-    ${response.portfolio}
-    ${response.email} 
-  `
+
+
+    <h2> Table of Contents </h2>
+    <li><a href="#title">Title</a></li>
+    <li><a href="#description">Description</a></li>  
+    <li><a href="#contents">Contents</a></li> 
+    <li><a href="#installation">Installation</a></li> 
+    <li><a href="#usage">Usage</a></li> 
+    <li><a href="#license">License</a></li> 
+    <li><a href="#contributor">Contributor</a></li>   
+    <li><a href="#tests">Tests</a></li> 
   
-    fs.writeFile("gen-README.md", usersInfo, function (err) {
+
+
+    <h2 id="description"> Description </h2>
+    <p>${response.description}</p>
+
+    <h2 id="description"> contents </h2>
+    <p>${response.contents}</p>
+
+    <h2 id="description"> installation </h2>
+    <p>${response.installation}</p>
+
+    <h2 id="description"> usage </h2>
+    <p>${response.usage}</p>
+
+    <h2 id="description"> license </h2>
+    <p>${response.license}</p>
+
+    <h2 id="description"> contributor </h2>
+    <p>${response.contributor}</p>
+
+    <h2 id="description"> tests </h2>
+    <p>${response.tests}</p>
+
+    <h2 id="email"> Email </h2>
+    <a href= "Email:${res.data.email}">${res.data.email}</a> 
+
+    <h2 id="portfolio"> Portfolio </h2>
+    <a href= "${res.data.blog}">Portfolio</a> `
+
+
+    fs.writeFile("gen-README.md", getInfo, function (err) {
   
       if (err) {
         return console.log(err);
